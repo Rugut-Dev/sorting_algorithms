22661f651b2dfbc071457d3cdaca357d6dd4b1e2
Kelvin Rugut <krugut36@gmail.com>
2023-12-16 11:16:52 +0300

    Betty compliancy

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -11,11 +11,11 @@ void bubble_sort(int *array, size_t size)
 	size_t i, j;
 	int temp, swap = 0;
 
-	for(i = 0; i < size; i++)
+	for (i = 0; i < size; i++)
 	{
-		for(j = 0; j < size - 1; j++)
+		for (j = 0; j < size - 1; j++)
 		{
-			if(array[j] > array[j + 1])
+			if (array[j] > array[j + 1])
 			{
 				temp = array[j];
 				array[j] = array[j + 1];
@@ -24,7 +24,7 @@ void bubble_sort(int *array, size_t size)
 				swap = 1;
 			}
 		}
-		if(swap == 0)
+		if (swap == 0)
 		{
 			return;
 		}
