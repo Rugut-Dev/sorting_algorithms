1a7846271b70f28e20513a5cfa0a4e5a203e3823
Kelvin Rugut <krugut36@gmail.com>
2023-12-16 11:05:44 +0300

    Bubble sort & Selection sort

---
+++ 2-selection_sort.c
@@ -0,0 +1,30 @@
+#include "sort.h"
+
+/**
+ * selection_sort - implements the selection sort algorithm
+ *
+ * @array: pointer to array of integers
+ * #size: size of @array
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j;
+	int minIndex, temp;
+
+	for(i = 0; i < size - 1; i++)
+	{
+		minIndex = i;
+		for(j = i + 1; j < size; j++)
+		{
+			if(array[j] < array[minIndex])
+			{
+				minIndex = j;
+			}
+		}
+
+		temp = array[minIndex];
+		array[minIndex] = array[i];
+		array[i] = temp;
+		print_array(array, size);
+	}
+}
--- sort.h
+++ sort.h
@@ -2,7 +2,6 @@
 #define SORT_H
 
 #include <stdio.h>
-#include <stdlib.h>
 
 /**
  * struct listint_s - Doubly linked list node
@@ -13,12 +12,13 @@
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
 void print_array(const int *array, size_t size);
+void selection_sort(int *array, size_t size);
 void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
 
