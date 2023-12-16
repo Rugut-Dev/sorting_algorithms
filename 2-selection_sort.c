d902627a28d771a7b525753d0183edba26891601
Kelvin Rugut <krugut36@gmail.com>
2023-12-16 11:45:33 +0300

    Selection sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -4,19 +4,19 @@
  * selection_sort - implements the selection sort algorithm
  *
  * @array: pointer to array of integers
- * #size: size of @array
+ * @size: size of @array
  */
 void selection_sort(int *array, size_t size)
 {
 	size_t i, j;
 	int minIndex, temp;
 
-	for(i = 0; i < size - 1; i++)
+	for (i = 0; i < size - 1; i++)
 	{
 		minIndex = i;
-		for(j = i + 1; j < size; j++)
+		for (j = i + 1; j < size; j++)
 		{
-			if(array[j] < array[minIndex])
+			if (array[j] < array[minIndex])
 			{
 				minIndex = j;
 			}
